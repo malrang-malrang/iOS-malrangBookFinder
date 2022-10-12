cb5222adb45e7ddf1e9312902b2ec64a2d3d42ab
malrang <ruehd7924@gmail.com>
2022-10-12 19:09:39 +0900

    ➕[add]: NetworkManager Test를 위한 MockURLProtocol 구현 #3

---
+++ iOS-malrangBookFinder/iOS-malrangBookFinderTests/DataTests/MockUrlProtocol.swift
@@ -0,0 +1,37 @@
+//
+//  MockUrlProtocol.swift
+//  iOS-malrangBookFinderTests
+//
+//  Created by 김동욱 on 2022/10/12.
+//
+
+import Foundation
+
+final class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            return
+        }
+
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+
+    override func stopLoading() {}
+}
