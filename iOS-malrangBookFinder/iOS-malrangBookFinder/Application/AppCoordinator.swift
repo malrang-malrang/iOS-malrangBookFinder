b2dba3e59c4899b4d9eecc8e6259d23d71686df6
malrang <ruehd7924@gmail.com>
2022-10-12 21:21:21 +0900

    ➕[add]: Coordinator protocol 구현및 AppCoordinator 구현  #5

---
+++ iOS-malrangBookFinder/iOS-malrangBookFinder/Application/AppCoordinator.swift
@@ -0,0 +1,32 @@
+//
+//  AppCoordinator.swift
+//  iOS-malrangBookFinder
+//
+//  Created by 김동욱 on 2022/10/12.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController { get }
+    var parentCoordinators: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
+}
+
+extension Coordinator {
+    func removeChild(_ coordinator: Coordinator) {
+        self.childCoordinators.removeAll()
+    }
+}
+
+final class AppCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    var parentCoordinators: Coordinator?
+    var childCoordinators: [Coordinator] = []
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {}
+}
