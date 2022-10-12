342fd312c054705f9d879164692b2b69b9dd8cfe
malrang <ruehd7924@gmail.com>
2022-10-12 15:16:05 +0900

    ✨[feat]: Requestable generateURL() 구현 #3

--- iOS-malrangBookFinder/iOS-malrangBookFinder/Data/NetworkManager/Requestable.swift
+++ iOS-malrangBookFinder/iOS-malrangBookFinder/Data/NetworkManager/Requestable.swift
@@ -11,5 +11,43 @@ protocol Requestable {
     var host: String { get }
     var path: String { get }
     var method: HttpMethod { get }
-    var queryParameters: Encodable? { get }
+    var queryParameters: Encodable { get }
 }
+
+extension Requestable {
+    func generateURL() -> Result<URL, NetworkError> {
+        let fullPath = "\(self.host)\(self.path)"
+        guard var urlComponent = URLComponents(string: fullPath) else {
+            return .failure(.urlComponetError)
+        }
+
+        switch self.generateQueryItems(at: self.queryParameters) {
+        case .success(let queryItems):
+            urlComponent.queryItems = queryItems
+        case .failure(let error):
+            return .failure(error)
+        }
+
+        guard let url = urlComponent.url else {
+            return .failure(.urlError)
+        }
+
+        return .success(url)
+    }
+
+    private func generateQueryItems(at queryParameters: Encodable) -> Result<[URLQueryItem], NetworkError> {
+        var urlQueryItems: [URLQueryItem] = []
+        switch queryParameters.toDictionary() {
+        case .success(let dictionaryData):
+            dictionaryData.forEach { key, value in
+                let queryItem = URLQueryItem(name: key, value: "\(value)")
+                urlQueryItems.append(queryItem)
+            }
+        case .failure(let error):
+            return .failure(error)
+        }
+
+        return .success(urlQueryItems)
+    }
+}
+
