48fdc5d00d9b42a1cde9f60a0afaf54dc9b11b0c
malrang <ruehd7924@gmail.com>
2022-10-14 17:09:05 +0900

    ➕[add]: ColorPalette eum Color 추가 #5

--- iOS-malrangBookFinder/iOS-malrangBookFinder/Utils/ColorPalette/ColorPalette.swift
+++ iOS-malrangBookFinder/iOS-malrangBookFinder/Utils/ColorPalette/ColorPalette.swift
@@ -8,5 +8,6 @@
 import UIKit
 
 enum ColorPalette {
-    static let malrangPink = #colorLiteral(red: 1, green: 0.7698566914, blue: 0.8562441468, alpha: 1)
+    static let malrangDeepPink = #colorLiteral(red: 1, green: 0.7698566914, blue: 0.8562441468, alpha: 1)
+    static let malrangPink = #colorLiteral(red: 1, green: 0.8737915158, blue: 0.9193537831, alpha: 1)
 }
