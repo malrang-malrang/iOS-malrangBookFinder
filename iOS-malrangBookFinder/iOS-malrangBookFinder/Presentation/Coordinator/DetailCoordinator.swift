51bf309661e42becc0c4f8017959baca26d59a2a
malrang <ruehd7924@gmail.com>
2022-10-14 17:25:32 +0900

    ➕[add]: DetailCoordinator 추가및 정의 #5

---
+++ iOS-malrangBookFinder/iOS-malrangBookFinder/Presentation/Coordinator/DetailCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  DetailCoordinator.swift
+//  iOS-malrangBookFinder
+//
+//  Created by 김동욱 on 2022/10/14.
+//
+
+import UIKit
+
+protocol DetailCoordinatorProtocol: Alertable {
+    func popDetailView()
+}
+
+final class DetailCoordinator: Coordinator, DetailCoordinatorProtocol {
+    var navigationController: UINavigationController
+    var parentCoordinators: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    private let bookSearchUseCase: BookListSearchUseCaseProtocol
+
+    init(
+        navigationController: UINavigationController,
+        parentCoordinators: Coordinator? = nil,
+        childCoordinators: [Coordinator] = [],
+        bookSearchUseCase: BookListSearchUseCaseProtocol
+    ) {
+        self.navigationController = navigationController
+        self.parentCoordinators = parentCoordinators
+        self.bookSearchUseCase = bookSearchUseCase
+    }
+
+    func start() {}
+
+    func popDetailView() {
+        self.navigationController.popViewController(animated: true)
+        self.parentCoordinators?.removeChild(self)
+    }
+}
